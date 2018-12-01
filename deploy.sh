f4655867e9313b1a334712b5a3b61468873710ff
Mark Somerville <my.fingers.today>
2018-12-01 14:26:30 +0000

    Update deploy script to use new 0f params

--- deploy.sh
+++ deploy.sh
@@ -2,12 +2,13 @@
 
 hostname=fingers.today
 domain=fingers.today
-sitepath=/data/0f/sites/${domain}/lib/
+base_dir=/data/0f/
+sitepath=${base_dir}/sites/${domain}/lib/
 
 if [ -z "$1" ]; then
   echo "Usage: deploy <gitobject>"
 else
-  revision_for_log=$(git rev-parse $1)
+  revision_for_log=$(git rev-parse $1) &&
   git archive -o deploy.tar.gz ${revision_for_log} &&
   scp deploy.tar.gz ${hostname}:${sitepath} &&
   rm deploy.tar.gz &&
@@ -15,7 +16,7 @@ else
     cd ${sitepath} &&
     tar -zxvf deploy.tar.gz &&
     rm deploy.tar.gz &&
-    0f ${domain} &&
+    sudo PATH=$PATH ${base_dir%/}/lib/bin/0f --base-dir ${base_dir} --domain ${domain} --fingerprint &&
     echo $(date): ${revision_for_log} >> deploy.log
   "
 fi
